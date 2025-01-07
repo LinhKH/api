c58f8813db00006943dacfe75c2f6387e8b655fe
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2025-01-07 23:23:28 +0700

    create a test api route

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
 
+import userRoutes from "./routes/user.route.js";
+
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((error) => {
-  console.log(error);
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
