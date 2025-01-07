b2ee3874274d9e7c8c124e6d62833599dc02e72f
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2025-01-08 00:20:26 +0700

    create signup api route

---
+++ controllers/auth.controller.js
@@ -0,0 +1,20 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+  const hashedPassword = await bcryptjs.hash(password, 10);
+  const newUser = new User({ name, email, password:hashedPassword });
+  try {
+    await newUser.save();
+    res.status(200).json({ message: "User created successfully!" });
+    
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+
+};
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 // Connect to MongoDB
 mongoose
@@ -20,6 +21,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
