f90aa085515887f14a0d59204261d5f0323e1d34
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2025-01-08 15:19:27 +0700

    api google auth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-import { handleError } from '../utils/error.js';
-import { StatusCodes } from 'http-status-codes';
-import jwt from 'jsonwebtoken';
+import { handleError } from "../utils/error.js";
+import { StatusCodes } from "http-status-codes";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username: name, email, password } = req.body;
@@ -11,23 +11,23 @@ export const signup = async (req, res, next) => {
     return next(handleError(400, "Please provide all fields"));
   }
   const hashedPassword = await bcryptjs.hash(password, 10);
-  const newUser = new User({ name, email, password:hashedPassword });
+  const newUser = new User({ name, email, password: hashedPassword });
   try {
     await newUser.save();
     res.status(200).json({ message: "User created successfully!" });
-    
   } catch (error) {
     next(error);
     // res.status(500).json(error.message);
   }
-
 };
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return next(handleError(StatusCodes.BAD_REQUEST, "Please provide all fields"));
+    return next(
+      handleError(StatusCodes.BAD_REQUEST, "Please provide all fields")
+    );
   }
 
   try {
@@ -35,15 +35,66 @@ export const signin = async (req, res, next) => {
     if (!validUser) {
       return next(handleError(404, "User not found"));
     }
-    const isValidPassword = await bcryptjs.compare(password, validUser.password);
+    const isValidPassword = await bcryptjs.compare(
+      password,
+      validUser.password
+    );
     if (!isValidPassword) {
       return next(handleError(StatusCodes.UNAUTHORIZED, "Invalid credentials"));
     }
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
     const { password: _, ...rest } = validUser._doc;
-    res.cookie('access_token', token, { httpOnly: true }).status(200).json({ message: "User signed in successfully!", rest });
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json({ message: "User signed in successfully!", rest });
+  } catch (error) {
+    next(error);
+  }
+};
 
+export const google = async (req, res, next) => {
+  const { name, email, photo } = req.body;
 
+  if (!name || !email) {
+    return next(
+      handleError(StatusCodes.BAD_REQUEST, "Please provide all fields")
+    );
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = await bcryptjs.hash(generatedPassword, 10);
+      const newUser = new User({
+        name,
+        email,
+        avatar: photo,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      });
+      const { password: _, ...rest } = newUser._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json({ message: "User signed in successfully!", rest });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    const { password: _, ...rest } = user._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(StatusCodes.OK)
+      .json({ message: "User signed in successfully!", rest });
   } catch (error) {
     next(error);
   }
