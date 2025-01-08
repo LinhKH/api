5305bfb7fd80bd2bb7bf6ee01280f32718f65466
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2025-01-08 12:29:20 +0700

    create sign in  api route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { handleError } from '../utils/error.js';
+import { StatusCodes } from 'http-status-codes';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username: name, email, password } = req.body;
@@ -19,4 +21,30 @@ export const signup = async (req, res, next) => {
     // res.status(500).json(error.message);
   }
 
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(handleError(StatusCodes.BAD_REQUEST, "Please provide all fields"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(handleError(404, "User not found"));
+    }
+    const isValidPassword = await bcryptjs.compare(password, validUser.password);
+    if (!isValidPassword) {
+      return next(handleError(StatusCodes.UNAUTHORIZED, "Invalid credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    const { password: _, ...rest } = validUser._doc;
+    res.cookie('access_token', token, { httpOnly: true }).status(200).json({ message: "User signed in successfully!", rest });
+
+
+  } catch (error) {
+    next(error);
+  }
 };
